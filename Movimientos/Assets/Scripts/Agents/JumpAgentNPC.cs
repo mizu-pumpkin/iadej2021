957764bdf8a6de2988f571c1a3301946bdcf0441
mizu-pumpkin <marisolisabel.zucca@um.es>
2021-05-31 22:11:59 +0200

    JumpAgent con arbitro ponderado

--- Movimientos/Assets/Scripts/Agents/JumpAgentNPC.cs
+++ Movimientos/Assets/Scripts/Agents/JumpAgentNPC.cs
@@ -13,7 +13,36 @@ public class JumpAgentNPC : AgentNPC
         this.favoriteTerrain = new List<string>();
         this.difficultTerrain = new List<string>();
         this.mass = 5;
-        this.maxSpeed = 10;
-        this.maxAcceleration = 5;
+        this.maxSpeed = 4;
+        this.maxAcceleration = 8;
     }
+
+    public override void LateUpdate()
+    {
+        steerings = new List<Steering>();
+        Steering accum = new Steering();
+
+        // Arbitro
+        foreach (SteeringBehaviour sb in this.steeringBehaviours) {
+            Steering steer = sb.GetSteering(this);
+            if (steer == null) continue;
+
+            if (sb is WallAvoid) {
+                steerings = new List<Steering>();
+                steerings.Add(steer);
+                //accum = steer;
+                break;
+            }
+
+            steer.linear *= sb.priority;
+            steer.angular *= sb.priority;
+            steerings.Add(steer);
+
+            accum.linear += steer.linear; // FIXME: sb.weight
+            accum.angular += steer.angular; // FIXME: sb.weight
+        }
+
+        //steerings.Add(accum);
+    }
+
 }
